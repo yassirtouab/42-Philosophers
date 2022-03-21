88c744039aff1ee34497e1f4e7b0324d892d3825
Yassir Touab <ytouab@lab3r6s1.42abudhabi.ae>
2022-03-21 11:44:46 +0400

    add

--- philosophers.h
+++ philosophers.h
@@ -6,16 +6,16 @@
 /*   By: ytouab <ytouab@student.42abudhabi.ae>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 16:11:50 by ytouab            #+#    #+#             */
-/*   Updated: 2022/03/20 17:45:41 by ytouab           ###   ########.fr       */
+/*   Updated: 2022/03/21 11:44:39 by ytouab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-# include "unistd.h"
-# include "stdlib.h"
-# include "stdio.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
 
 #endif
