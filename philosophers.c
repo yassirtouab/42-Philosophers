586e10f9f158945ec016f1298957bcbe9d63562b
Yassir Touab <ytouab@lab3r6s1.42abudhabi.ae>
2022-04-10 21:19:31 +0400

    Philo struct

--- philosophers.c
+++ philosophers.c
@@ -6,16 +6,21 @@
 /*   By: ytouab <ytouab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 16:15:09 by ytouab            #+#    #+#             */
-/*   Updated: 2022/04/09 09:41:40 by ytouab           ###   ########.fr       */
+/*   Updated: 2022/04/10 21:19:08 by ytouab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+void	take_args(t_data *data, int ac, char **av)
+{}
+
 int	main(int ac, char **av)
 {
 	t_data	data;
+	size_t	i;
 
+	i = 0;
 	if (ac == 6 || ac == 5)
 	{
 	}
